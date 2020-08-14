1704f9f988ed96fdb52328ea6b526b048928991e
Frantz Fortunat <frantzfortunat@gmail.com>
2020-08-14 17:40:08 +0200

    started front, added banner + navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,22 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+console.log("Hello from app/javascript/packs/application.js!");
 
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
+
+import { loadDynamicBannerText } from '../components/banner';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  // [...]
+  loadDynamicBannerText();
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
